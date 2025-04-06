0762a8220c23166d3d8fc151d638f773ac20a117
etienne <laurentlionel20@hotmail.com>
2025-04-06 22:26:43 +0200

    [Update] dependencies

---
+++ blazor/App.cs
@@ -0,0 +1,5 @@
+namespace blazor.Components;
+public partial class App 
+{
+
+}
--- blazor/Program.cs
+++ blazor/Program.cs
@@ -1,5 +1,6 @@
 using blazor.Components;
 using blazor.services;
+using csrf;
 using DotNetEnv;
 using Microsoft.AspNetCore.Components.Authorization;
 using Prometheus;
@@ -13,35 +14,65 @@ builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
+builder.Services.AddSingleton<CsrfService>();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthProvider>();
 builder.Services.AddScoped<IAuthService,AuthService>();
 builder.Services.AddScoped<IProgramService,ProgramService>();
 builder.Services.AddScoped<IReportService,ReportService>();
+builder.Services.AddScoped<CsrfContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Error", createScopeForErrors: true);
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+app.Use(
+    async (context, next) =>
+    {
 
+    CsrfService csrfService = context.RequestServices.GetRequiredService<CsrfService>();
+    IConfiguration configuration = context.RequestServices.GetRequiredService<IConfiguration>();
+    string CSRF_COOKIE_NAME =configuration["CSRF_COOKIE_NAME"]??throw new Exception("Missing configuration CSRF_COOKIE_NAME"); 
+    string CSRF_HEADER_NAME =configuration["CSRF_HEADER_NAME"]??throw new Exception("Missing configuration CSRF_HEADER_NAME"); 
+    var (cookieToken, requestToken) = csrfService.GenerateTokens();
+    var endpoint = context.GetEndpoint();
+
+    if (endpoint?.Metadata.GetMetadata<RequireCsrfTokenAttribute>() != null )
+    {
+
+    context.Response.Cookies.Append(
+        CSRF_COOKIE_NAME,
+        cookieToken,
+        new CookieOptions
+        {
+        HttpOnly = true,
+        Secure = true,
+        SameSite = SameSiteMode.Strict
+        });
+
+    }
+    string cookie = context.Request.Cookies[CSRF_COOKIE_NAME] ?? "";
+    context.Items[CSRF_HEADER_NAME] = csrfService.ComputeHmac(cookie);
+
+    await next();
+    });
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+  .AddInteractiveServerRenderMode();
 
-
-app.MapMetrics(); 
-app.UseStatusCodePagesWithRedirects("/404");
-app.Run();
+  app.MapMetrics(); 
+  app.UseStatusCodePagesWithRedirects("/404");
+  app.Run();
